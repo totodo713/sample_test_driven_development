bf62bde751353798817abb8198f70d073ceb63cd
Satoshi Yamagami <s.yamagami.666@gmail.com>
2020-03-24 15:49:48 +0900

    temp:第12章終了時点

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -31,4 +31,14 @@ public class MoneyTest {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
   }
+
+  @Test
+  @DisplayName("単純な足し算")
+  public void testSimpleAdditon() {
+    Money five = Money.dollar(5);
+    Expression sum = five.plus(five);
+    Bank bank = new Bank();
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
+  }
 }
