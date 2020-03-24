bbfa7f67aab9dfb805ba7647be79f2aacb40b0f9
Satoshi Yamagami <s.yamagami.666@gmail.com>
2020-03-24 11:28:09 +0900

    temp:第2章終了時点

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -5,14 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Money")
+@DisplayName("通貨テスト")
 public class MoneyTest {
 
   @Test
   @DisplayName("掛け算")
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(5 * 2, five.amount);
+    Dollar actual = five.times(2);
+    assertEquals(5 * 2, actual.amount);
+    actual = five.times(3);
+    assertEquals(5 * 3, actual.amount);
   }
 }
